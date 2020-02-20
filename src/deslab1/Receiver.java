74b9e5337b2396218eb1fbec1907ec87031bd64a
Vinayak Desai <34840257+vinayalaw@users.noreply.github.com>
2020-02-20 09:42:49 -0500

    Update Receiver.java

--- src/deslab1/Receiver.java
+++ src/deslab1/Receiver.java
@@ -9,18 +9,21 @@ import javax.crypto.spec.SecretKeySpec;
 
 /**
  *
- * 
+ *
  * @author vinay
  */
 public class Receiver {
     private static Socket socket = null;
     private static ServerSocket server = null;
+    private static DataOutputStream dOut = null;
     private static DataInputStream dIn = null;
     public static void main(String[] args) {
         int port = 8888;
         String plain="", keyStr="", ciphertext="";
         Scanner in = new Scanner(System.in);
-
+        DesEncrypter encrypter = null;
+        SecretKey key = null;
+        
         try{
             //connect
             server = new ServerSocket(port);
@@ -28,34 +31,74 @@ public class Receiver {
             System.out.println("Connected!");
             //receive a message
             dIn = new DataInputStream(socket.getInputStream());
-            ciphertext = dIn.readUTF();
-            System.out.println("We got the message!");
-            //get the key
+            dOut = new DataOutputStream(socket.getOutputStream());
             System.out.println("Enter Key: ");
             keyStr = in.nextLine();
-            //close open vars
+            byte[] decodedKey = Base64.getDecoder().decode(keyStr);
+            key = new SecretKeySpec(decodedKey, 0, decodedKey.length, "DES");
+            encrypter = new DesEncrypter(key);           
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        
+        while(!plain.equals("exit")){
+            /************Getting a message**********/
+            try{
+                ciphertext = dIn.readUTF();
+                System.out.println("We got the message!");
+                //decrypt the message
+                System.out.println("Plain: " + plain);
+                System.out.println("Cipher: " + ciphertext);
+                try {                                        
+                    plain = encrypter.decrypt(ciphertext);
+                } catch (Exception ex) {
+                    System.out.println("Encryption error!");
+                }
+                //display plaintext and ciphertext
+                
+            }
+            catch(IOException e){
+                System.out.println(e);
+            }
+            plain = "";
+            /*******Sending a message*********/
+            //get message
+            System.out.println("Please write your message then type enter: ");
+            while(plain.equals("")){
+                plain = in.nextLine();
+                try{
+                    ciphertext = encrypter.encrypt(plain);
+                }
+                catch(Exception e){
+                    System.out.println(e);
+                }
+            }
+            try{
+                //send the message
+                dOut.writeUTF(ciphertext);
+                dOut.flush();
+                //give the key
+                System.out.println("Here is the key: ");
+                System.out.println(Base64.getEncoder().encodeToString(key.getEncoded())+"\n\n");
+                
+                //display plaintext and ciphertext
+                System.out.println("Plain: " + plain);
+                System.out.println("Cipher: " + ciphertext);
+            }
+            catch(IOException e){System.out.println("error sending message!");}
+            
+            
+            plain = "";
+        }
+        
+        try{
             socket.close();
             in.close();
             dIn.close();
         }
         catch(IOException e){
             System.out.println(e);
-            System.exit(1);
-        }      
-        
-        //decrypt the message
-        SecretKey key = null;
-        try {
-            byte[] decodedKey = Base64.getDecoder().decode(keyStr);
-            key = new SecretKeySpec(decodedKey, 0, decodedKey.length, "DES");
-            DesEncrypter encrypter = new DesEncrypter(key);
-            plain = encrypter.decrypt(ciphertext);
-        } catch (Exception ex) {
-            System.out.println("Encryption error!");
         }
-        
-        //display plaintext and ciphertext
-        System.out.println("Plain: " + plain);
-        System.out.println("Cipher: " + ciphertext);
-    }  
+    }
 }
